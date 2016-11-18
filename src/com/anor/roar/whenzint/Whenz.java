82410ccdbc060a71f0864da1e230fb991799ca0c
nick <nick@Cerberus>
2016-11-17 21:03:42 -0500

    Cleanup unused class

--- src/com/anor/roar/whenzint/Whenz.java
+++ src/com/anor/roar/whenzint/Whenz.java
@@ -2,20 +2,8 @@ package com.anor.roar.whenzint;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.LinkedList;
-import java.util.List;
 
-import com.anor.roar.whenzint.actions.ChainAction;
-import com.anor.roar.whenzint.actions.ExitAction;
-import com.anor.roar.whenzint.actions.LaunchWindowAction;
-import com.anor.roar.whenzint.actions.PrintAction;
-import com.anor.roar.whenzint.actions.SetCurrentObject;
-import com.anor.roar.whenzint.actions.TriggerEventAction;
-import com.anor.roar.whenzint.conditions.EventCondition;
 import com.anor.roar.whenzint.parser.WhenzParser;
-import com.anor.roar.whenzint.patterns.Pattern;
 
 public class Whenz {
 
