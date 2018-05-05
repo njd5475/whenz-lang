21d68dcf4b62bbe82e406a487c1eb1ccd7e7bba4
Nick <njd5475@gmail.com>
2018-05-05 08:01:06 -0400

    Fixes gradle dependencies for unit tests.

---
+++ test/com/anor/roar/whenzint/WhenzTests.java
@@ -0,0 +1,16 @@
+package com.anor.roar.whenzint;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WhenzTests {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testHelloWorld() {
+  }
+
+}
