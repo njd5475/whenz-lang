fe867d8e2f55403772a6eaea07c6651e9c2322e6
nick <njd5475@gmail.com>
2020-10-16 00:22:05 -0400

    Add version calculation script, days since epoch.

---
+++ version.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'active_support/core_ext'
+
+readVal = ARGV.shift if ARGV[0]
+
+puts Time.at(readVal.to_i * 60 * 60 * 24).utc.beginning_of_day if readVal
+
+now = Time.now.utc          # Current time
+
+puts now.to_i / 60 / 60 / 24 if not readVal
+
